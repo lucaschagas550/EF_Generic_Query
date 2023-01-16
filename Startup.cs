76c3c9a80dda9c7c72f8b633f09247792dc97b88
DESKTOP-M79GPG6\lucas <Lucasandrade595@gmail.com>
2023-01-16 17:59:16 -0300

    feat: configure startup

---
+++ Startup.cs
@@ -0,0 +1,34 @@
+﻿namespace EF.Generic_Query.API
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen();
+        }
+
+        public void Configure(WebApplication app, IWebHostEnvironment env)
+        {
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+            app.MapControllers();
+        }
+    }
+}
